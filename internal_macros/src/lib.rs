fd3e3883744df1041c1c4db17b80c44565425c5c
Finn H <f.heintzmann@ostfalia.de>
2024-12-06 14:02:19 +0100

    implement boilerplate

--- internal_macros/src/lib.rs
+++ internal_macros/src/lib.rs
@@ -1,6 +1,8 @@
-use proc_macro::TokenStream;
+use syn::parse_macro_input;
 
 #[proc_macro]
-pub fn link_cranelift(_tokens: TokenStream) -> TokenStream {
-    todo!()
+pub fn link_cranelift(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = parse_macro_input!(input as proc_macro2::TokenStream);
+    println!("ast: {:#?}", ast);
+    todo!();
 }
