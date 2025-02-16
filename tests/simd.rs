a5659d3758bd257f155a0ad05c07d9ca159c6f7c
Finn H <f.heintzmann@ostfalia.de>
2025-02-16 23:00:03 +0100

    simd test working

--- tests/simd.rs
+++ tests/simd.rs
@@ -1,33 +1,32 @@
 #![cfg_attr(feature = "simd", feature(portable_simd))]
 #![cfg(feature = "simd")]
 use anita::compile_expression;
-use std::{assert_eq, println, simd::f32x4};
-
-#[repr(align(16))]
-struct AlignedF32x4(f32x4);
+use std::{assert_eq, print, simd::f32x4};
 
 #[test]
 fn it_works() {
-    let function = {
+    let (cf, function) = {
         use anita_core::function_manager::NoFunctions;
-        use anita_core::jit::{compiled_function::CompiledFunction, JIT};
+        use anita_core::jit::JIT;
         use std::mem;
         let mut jit = JIT::<f32x4, NoFunctions>::default();
-        match jit.compile("x", &[stringify!(x)]) {
+        match jit.compile("x*x", &[stringify!(x)]) {
             Ok(code_ptr) => {
                 let function_pointer = unsafe {
-                    mem::transmute::<*const u8, fn(f32x4) -> f32x4>(
+                    mem::transmute::<*const u8, extern "C" fn(f32x4) -> f32x4>(
                         code_ptr,
                     )
                 };
                 let memory_region = jit.dissolve();
-                Ok(CompiledFunction::new(memory_region, function_pointer))
+                Ok((memory_region, function_pointer))
             }
             Err(e) => Err(e),
         }
     }.expect("compilation failed");
     let x = f32x4::splat(5.0);
     let result = function(x);
+    println!("result: {:?}\nexpected: {:?}", result, x);
     assert_eq!(x, result);
-    // panic!("panic for diagnostics");
+    // drop(cf);
+    panic!("panic for diagnostics");
 }
