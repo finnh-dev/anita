a48128de64920256eee1e2cd2611690fd8f46dc7
Finn H <f.heintzmann@ostfalia.de>
2025-02-16 23:06:54 +0100

    fix calling convention error

--- tests/simd.rs
+++ tests/simd.rs
@@ -1,32 +1,13 @@
 #![cfg_attr(feature = "simd", feature(portable_simd))]
 #![cfg(feature = "simd")]
 use anita::compile_expression;
-use std::{assert_eq, print, simd::f32x4};
+use std::{assert_eq, simd::f32x4};
 
 #[test]
 fn it_works() {
-    let (cf, function) = {
-        use anita_core::function_manager::NoFunctions;
-        use anita_core::jit::JIT;
-        use std::mem;
-        let mut jit = JIT::<f32x4, NoFunctions>::default();
-        match jit.compile("x*x", &[stringify!(x)]) {
-            Ok(code_ptr) => {
-                let function_pointer = unsafe {
-                    mem::transmute::<*const u8, extern "C" fn(f32x4) -> f32x4>(
-                        code_ptr,
-                    )
-                };
-                let memory_region = jit.dissolve();
-                Ok((memory_region, function_pointer))
-            }
-            Err(e) => Err(e),
-        }
-    }.expect("compilation failed");
+    let function = compile_expression!("x * x", (x) -> f32x4).expect("compilation failed");
     let x = f32x4::splat(5.0);
     let result = function(x);
-    println!("result: {:?}\nexpected: {:?}", result, x);
-    assert_eq!(x, result);
-    // drop(cf);
-    panic!("panic for diagnostics");
+    let expected = x * x;
+    assert_eq!(expected, result);
 }
